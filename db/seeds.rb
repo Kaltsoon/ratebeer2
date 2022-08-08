a343ab5efa3bd7dd0c987a3f1fabd4c4e1616272
Kalle Ilves <kalleilv@gmail.com>
2022-08-08 12:08:55 +0300

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+b1 = Brewery.create name:"Koff", year:1897
+b2 = Brewery.create name:"Malmgard", year:2001
+b3 = Brewery.create name:"Weihenstephaner", year:1040
+
+b1.beers.create name:"Iso 3", style:"Lager"
+b1.beers.create name:"Karhu", style:"Lager"
+b1.beers.create name:"Tuplahumala", style:"Lager"
+b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
+b2.beers.create name:"X Porter", style:"Porter"
+b3.beers.create name:"Hefeweizen", style:"Weizen"
+b3.beers.create name:"Helles", style:"Lager"
